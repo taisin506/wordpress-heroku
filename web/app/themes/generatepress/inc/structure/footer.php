77d3956b6e88d253dc127f2381aba13ddffcc795
taisin506 <76686960+taisin506@users.noreply.github.com>
2020-12-30 00:56:51 +0800

    1

--- web/app/themes/generatepress/inc/structure/footer.php
+++ web/app/themes/generatepress/inc/structure/footer.php
@@ -86,13 +86,7 @@ if ( ! function_exists( 'generate_add_footer_info' ) ) {
 	function generate_add_footer_info() {
 		$copyright = sprintf(
 			'<span class="copyright"></span> &bull; %4$s <a href="%3$s"%6$s>%5$s</a>',
-			date( 'Y' ), // phpcs:ignore
-			get_bloginfo( 'name' ),
-			esc_url( 'https://generatepress.com' ),
-			_x( 'Built with', 'GeneratePress', 'generatepress' ),
-			__( 'GeneratePress', 'generatepress' ),
-			'microdata' === generate_get_schema_type() ? ' itemprop="url"' : ''
-		);
+			;
 
 		echo apply_filters( 'generate_copyright', $copyright ); // phpcs:ignore
 	}
