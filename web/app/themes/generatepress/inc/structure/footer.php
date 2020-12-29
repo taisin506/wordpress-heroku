2b98f08001244d92b6942f8ed0c6604a6d4fbeef
taisin506 <76686960+taisin506@users.noreply.github.com>
2020-12-30 01:04:26 +0800

    1

--- web/app/themes/generatepress/inc/structure/footer.php
+++ web/app/themes/generatepress/inc/structure/footer.php
@@ -86,7 +86,13 @@ if ( ! function_exists( 'generate_add_footer_info' ) ) {
 	function generate_add_footer_info() {
 		$copyright = sprintf(
 			'<span class="copyright"></span> &bull; %4$s <a href="%3$s"%6$s>%5$s</a>',
-			;
+			date( 'Y' ), // phpcs:ignore
+			get_bloginfo( 'name' ),
+			esc_url( 'https://generatepress.com' ),
+			_x( 'All characters on this site are 18+. It's drawn material, even if they don't look like it, they are all 18+. Please report any concerns you might have here.', 'GeneratePress', 'generatepress' ),
+			__( 'GeneratePress', 'generatepress' ),
+			'microdata' === generate_get_schema_type() ? ' itemprop="url"' : ''
+		);
 
 		echo apply_filters( 'generate_copyright', $copyright ); // phpcs:ignore
 	}
